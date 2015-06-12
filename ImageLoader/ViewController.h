d5b6f0a95f6eba7ef960f22e8fe36b397f1741d4
rainlesvio <rainlesvio@163.com>
2015-06-12 09:18:00 +0800

    Initial Commit

---
+++ ImageLoader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageLoader
+//
+//  Created by wuxueying on 6/12/15.
+//  Copyright (c) 2015 wuxueying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ImageLoader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ImageLoader
+//
+//  Created by wuxueying on 6/12/15.
+//  Copyright (c) 2015 wuxueying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
